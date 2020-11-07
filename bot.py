adfcdbf05f3028e9f054a1ca4663d10ed54e1b15
notdoods <khchao@uci.edu>
2020-11-07 11:12:40 -0800

    Extendo Pinz
    Add channels to pins + QoL Changes

--- bot.py
+++ bot.py
@@ -15,13 +15,36 @@ priceClient = Client(coinbase_key,coinbase_secret)
 cryptoCheck = {}
 defaultChannel = -1
 
+pinsChannelID = 749866750069178430
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    if str(reaction) == '📌':
+        pinsChannel = bot.get_channel(pinsChannelID)
+        message = reaction.message
+        if message.content != "":
+            if len(message.embeds) == 0:
+                botMessage = f'"{message.content}"-{message.author.name}'
+                await pinsChannel.send(botMessage)
+            else:
+                urlE = message.embeds[0].url
+                await pinsChannel.send(urlE)
+        else:
+            if len(message.attachments) > 0:
+                url = message.attachments[0].url
+                await pinsChannel.send(url)
+
+
+
 @bot.command(name='default', help='Sets the default channel to send messages for big changes of !check currencies'
                                         'to current channel')
 async def changeChannel(ctx):
     global defaultChannel
     defaultChannel = ctx.channel.id
     channel = bot.get_channel(defaultChannel)
-    await ctx.send('Changed channel for updates to: ' + str(ctx.channel))
+    await ctx.send('Changed channel for updates to: ' + str(ctx.channel), delete_after = 5.0)
+    await ctx.message.delete(delay=5.0)
 
 @bot.command(name='add', help='!add [crypto] - Inputs valid cryptocurrency for checking for huge changes')
 async def addCrypto(ctx, crypt: str, currency='USD'):
@@ -29,19 +52,23 @@ async def addCrypto(ctx, crypt: str, currency='USD'):
         cPair = '{}-{}'.format(crypt.upper(),currency.upper())
         product = priceClient.get_spot_price(currency_pair=cPair)
         cryptoCheck[crypt.upper()] = product["amount"]
-        await ctx.send('```Added: ' + crypt.upper()+'```')
+        await ctx.send('```Added: ' + crypt.upper()+'```', delete_after = 5.0)
     except:
         await ctx.send("Error: Not a valid input. Check !currencies for a list of available"
-                       "valid trading cryptocurrencies")
+                       "valid trading cryptocurrencies", delete_after = 5.0)
+    finally:
+        await ctx.message.delete(delay=5.0)
 
 @bot.command(name='delete', help='!delete [crypto] - Deletes cryptocurrency from checking for huge changes (Opposite effect of !add)')
 async def delCrypto(ctx, crypt: str):
     try:
         cryptoCheck.pop(crypt.upper())
-        await ctx.send('```Deleted: ' +crypt.upper()+'```')
+        await ctx.send('```Deleted: ' +crypt.upper()+'```', delete_after = 5.0)
     except:
         await ctx.send("Error: Not a valid input. Check !currencies for a list of available"
-                       "valid trading cryptocurrencies")
+                       "valid trading cryptocurrencies", delete_after = 5.0)
+    finally:
+        await ctx.message.delete(delay=5.0)
 
 @bot.command(name='check', help="Manually checks current prices of !add'ed cryptocurrencies")
 async def checkCrypto(ctx):
@@ -57,30 +84,38 @@ async def checkCrypto(ctx):
 
 @tasks.loop(seconds=15)
 async def updateDict():
-    for k in cryptoCheck.keys():
-        cPair = k+'-USD'
-        price = priceClient.get_spot_price(currency_pair=cPair)
-        cryptoCheck[k] = price['amount']
+    try:
+        for k in cryptoCheck.keys():
+            cPair = k+'-USD'
+            price = priceClient.get_spot_price(currency_pair=cPair)
+            cryptoCheck[k] = price['amount']
+    except:
+        print(f'There was an error in updateDict at {client.get_time()["iso"][11:-1]}, check to see if '
+              f'the program still runs after catching an exception')
 
 @tasks.loop(minutes=5)
 async def checkChanges():
     global defaultChannel
     cryptoPercentage = {}
     concatenatedString = '```BIG CHANGES HAVE OCCURRED:\n'
-    for k in cryptoCheck.keys():
-        cPair = k+'-USD'
-        product = client.get_product_24hr_stats(cPair)
-        price = cryptoCheck[k]
-        open24 = product['open']
-        percentage = round((((float(price) - float(open24))/float(open24))*100),3)
-        if (percentage >= 2.5) or (percentage <= -2.5):
-            cryptoPercentage[k] = percentage
-    if len(cryptoPercentage) > 0:
-        for k,v in cryptoPercentage.items():
-            concatenatedString += f'{k}: 24 Hour Percentage {v}%\n'
-        channel = bot.get_channel(defaultChannel)
-        await channel.send(concatenatedString[0:-1]+'```')
-        await asyncio.sleep(3600)
+    try:
+        for k in cryptoCheck.keys():
+            cPair = k+'-USD'
+            product = client.get_product_24hr_stats(cPair)
+            price = cryptoCheck[k]
+            open24 = product['open']
+            percentage = round((((float(price) - float(open24))/float(open24))*100),3)
+            if (percentage >= 2.5) or (percentage <= -2.5):
+                cryptoPercentage[k] = percentage
+        if len(cryptoPercentage) > 0:
+            for k,v in cryptoPercentage.items():
+                concatenatedString += f'{k}: 24 Hour Percentage {v}%\n'
+            channel = bot.get_channel(defaultChannel)
+            await channel.send(concatenatedString[0:-1]+'```')
+            await asyncio.sleep(3600)
+    except:
+        print(f'There was an error in checkDict at {client.get_time()["iso"][11:-1]}, check to see if '
+              f'the program still runs after catching an exception')
 
 @bot.command(name='time', help='Displays current server time(Coinbase)')
 async def server_time(ctx):
@@ -118,6 +153,8 @@ async def stats24Hours(ctx, crypt: str, currency='USD'):
     except:
         await ctx.send("Error: Not a valid input. Check !currencies for a list of available"
                        "valid trading cryptocurrencies")
+    finally:
+        await ctx.message.delete(delay=5.0)
 
 
 @stats24Hours.error
